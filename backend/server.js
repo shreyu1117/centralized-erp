ef2db2e7b810ca5cbc96f85f3abaa5f40fb22d44
shreyu1117 <167845318+shreyu1117@users.noreply.github.com>
2026-01-05 14:19:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,10 @@ const app=express();
 app.use(express.json());
 app.get('/',(req,res)=>res.send('ERP API running'));
 app.listen(4000,()=>console.log('API running on 4000'));
+
+//git deploy 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
