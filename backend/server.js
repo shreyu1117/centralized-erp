d106c995e94b37cd4705e529a8a4085623df5825
shreyu1117 <167845318+shreyu1117@users.noreply.github.com>
2026-01-05 14:23:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,10 @@ const app=express();
 app.use(express.json());
 app.get('/',(req,res)=>res.send('ERP API running'));
 app.listen(4000,()=>console.log('API running on 4000'));
-
+/* ✅ TEST BACKEND API (ADD THIS) */
+app.get("/health", (req, res) => {
+  res.json({ status: "OK", message: "Backend is running" });
+});
 //git deploy 
 const PORT = process.env.PORT || 5000;
 
